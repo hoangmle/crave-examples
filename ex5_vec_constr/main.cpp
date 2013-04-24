52fc8bbae6b46eb1b1dc72f1c30d3920c6039da3
Tino Flenker <flenkium@informatik.uni-bremen.de>
2013-04-24 15:30:44 +0200

    add new placeholder from new expression architecture in example 5.

--- ex5_vec_constr/main.cpp
+++ ex5_vec_constr/main.cpp
@@ -1,16 +1,17 @@
 #include <crave/ConstrainedRandom.hpp>
+#include <crave/Constraint.hpp>
 
 using crave::rand_obj;
 using crave::randv;
 using crave::rand_vec;
-using crave::_i;
+using crave::placeholder;
 
 #define IF_THEN(a, b) !(a) || (b)
 #define IF_THEN_ELSE(a, b, c) (!(a) || (b)) && ((a) || (c))
 
 class item : public rand_obj {
 public:
-	item(rand_obj* parent = 0) : rand_obj(parent), src_addr_vec(this), dest_addr_vec(this), data_vec(this), tmp(this) {
+	item(rand_obj* parent = 0) : _i(), rand_obj(parent), src_addr_vec(this), dest_addr_vec(this), data_vec(this), tmp(this) {
 		tmp.range(5, 10);
 
 		constraint(src_addr_vec().size() == reference(tmp));
@@ -44,6 +45,7 @@ public:
 		return os; 
 	}
 
+    placeholder _i;
 	rand_vec<uint> src_addr_vec;
 	rand_vec<uint> dest_addr_vec;
 	rand_vec<uint> data_vec;
