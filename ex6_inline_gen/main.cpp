17438d7e96dae40e9083834f9cbaa1b8074525a4
Hoang M. Le <hle@informatik.uni-bremen.de>
2013-11-07 13:43:10 +0100

    updated crave, dependencies, and examples

--- ex6_inline_gen/main.cpp
+++ ex6_inline_gen/main.cpp
@@ -1,12 +1,12 @@
 #include <crave/ConstrainedRandom.hpp>
 
 using crave::randv;
-using crave::DefaultGenerator;
+using crave::Generator;
 
 int main (int argc , char *argv[]) {
 	randv<int> x(NULL);
 	randv<int> y(NULL);
-	DefaultGenerator gen;
+	Generator gen;
 	gen
 		(x() >= 0 && y() >= 0)
 		(x() + y() == 4)
