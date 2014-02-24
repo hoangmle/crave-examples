b19803fe4dd9a157b54c9aa17c45b8230ffe1b4f
Hoang M. Le <hle@informatik.uni-bremen.de>
2014-02-24 16:16:00 +0100

    updated crave, update ALU12 to show how to specify distribution for SystemC types

--- ALU/ALU12.cpp
+++ ALU/ALU12.cpp
@@ -8,6 +8,10 @@ using crave::randv;
 using sc_dt::sc_bv;
 using sc_dt::sc_uint;
 
+using crave::dist;
+using crave::distribution;
+using crave::reference;
+
 struct ALU12 : public rand_obj {
   randv< sc_bv<2> >  op ;
   randv< sc_uint<12> > a, b ;
@@ -15,6 +19,9 @@ struct ALU12 : public rand_obj {
   ALU12(rand_obj* parent = 0)
   : rand_obj(parent), op(this), a(this), b(this)
   {
+    randv<short> tmp(NULL);
+    constraint ( dist(tmp(), distribution<short>::simple_range(0, 3)) );
+    constraint ( tmp() == op() );    
     constraint ( (op() != 0x0) || ( 4095 >= a() + b() ) );
     constraint ( (op() != 0x1) || ((4095 >= a() - b()) && (b() <= a()) ) );
     constraint ( (op() != 0x2) || ( 4095 >= a() * b() ) );
@@ -40,6 +47,7 @@ int sc_main (int argc, char** argv)
   std::cout << "first: " << timer.elapsed() << "\n";
   for (int i=0; i<1000; ++i) {
     c.next();
+    std::cout << c << std::endl;
   }
   std::cout << "complete: " << timer.elapsed() << "\n";
   return 0;
