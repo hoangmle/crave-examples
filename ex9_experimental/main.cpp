98a3802befbaedbee16f334d788e6af71fde8ef3
Hoang M. Le <hle@informatik.uni-bremen.de>
2015-05-28 17:36:36 +0200

    added barn_drawer example + updated ex9

--- ex9_experimental/main.cpp
+++ ex9_experimental/main.cpp
@@ -1,103 +1,61 @@
 #include <crave/ConstrainedRandom.hpp>
 #include <crave/experimental/Experimental.hpp>
 
-#include <bitset>
- 
-struct my_covergroup : public crave::crv_covergroup {
-  CRV_VARIABLE(unsigned, x);
-  CRV_VARIABLE(unsigned, y);
+using namespace crave;
 
-  CRV_COVERPOINT(cp1);
-  CRV_COVERPOINT(cp2);
-  CRV_COVERPOINT(cp3);
-  CRV_COVERPOINT(cp4);
+struct irqmp_regs : public crv_sequence_item {
+  crv_variable<unsigned> level_reg { "level_reg" };
+  crv_variable<unsigned> force_reg { "force_reg" };
+
+  crv_constraint level_reg_cstr { "level_reg_cstr" };
+  crv_constraint force_reg_cstr { "force_reg_cstr" }; 
+
+  crv_constraint dist_cstr { "dist_str" };
+
+	irqmp_regs(crv_object_name) { 
+    level_reg_cstr = { level_reg() < (1 << 16), (level_reg() & 1) == 0 };
+    force_reg_cstr = { (force_reg() & 0xFFFF0001) == 0 };	
+    
+    dist_cstr = {
+      dist(level_reg(), distribution<unsigned>::simple_range(1, (1 << 16) - 1)),
+      dist(force_reg(), distribution<unsigned>::simple_range(1, (1 << 16) - 1))
+    };
+	}
+};
+ 
+struct irqmp_covergroup : public crv_covergroup {
+  crv_variable<unsigned> lr { "lr" };
+  crv_variable<unsigned> fr { "fr" };
+
+  crv_coverpoint fwd_lvl_1 { "fwd_lvl_1" };
+  crv_coverpoint fwd_lvl_0 { "fwd_lvl_0" };
    
-  my_covergroup(crave::crv_object_name) { 
-    for (int i = 0; i < 16; i++) {
-      cp1.bins( (x() >> i) == 1 );
-      cp2.bins( ((y() >> i) & 1) != 0 );
-      cp3.bins( ((x() >> i) == 1) && (((y() >> i) & 1) != 0) );
-      cp4.bins( bitcount(x()) == i );
+  expression forced_lvl_1() { return make_expression(fr() &  lr()); }
+  expression forced_lvl_0() { return make_expression(fr() & ~lr()); }
+
+  irqmp_covergroup(crv_object_name) { 
+    for (int k = 1; k < 16; k++) {
+      fwd_lvl_1.bins( (((forced_lvl_1() >> k) & 1) == 1) && (forced_lvl_1() < (2 << k)) );
+      fwd_lvl_0.bins( (((forced_lvl_0() >> k) & 1) == 1) && (forced_lvl_0() < (2 << k)) && (forced_lvl_1() == 0) );
     }  
   }
-};
-
-struct irqmp_item : public crave::crv_sequence_item {
-  CRV_VARIABLE(unsigned, pending); 
-  CRV_VARIABLE(unsigned, priority); 
-  
-  CRV_CONSTRAINT(pending_constr);
-  CRV_CONSTRAINT(priority_constr);
-
-	irqmp_item(crave::crv_object_name) { 
-	  pending_constr = { crave::dist(pending(), crave::distribution<unsigned>::simple_range(0, 1 << 16)) }; 
-	  priority_constr = { crave::dist(priority(), crave::distribution<unsigned>::simple_range(0, 1 << 16)) };
-	}   
-
-  void print() {
-    std::cout << "pending  = " << pending << " # " << std::bitset<32>(pending) << std::endl;
-    std::cout << "priority = " << priority << " # " << std::bitset<32>(priority) << std::endl;
-  }
-};
-
-template<int N>
-struct item : public crave::crv_sequence_item {
-  CRV_VARIABLE(unsigned, x); // crv_variable<unsigned> x { "x" };
-  CRV_VARIABLE(unsigned, y); // crv_variable<unsigned> y { "y" };
-  CRV_ARRAY(unsigned, N, arr); // crv_array<unsigned, N> arr { "arr" };
-  
-  CRV_CONSTRAINT(sum_arr, arr.sum() == 20); // crv_constraint sum_arr { "sum_arr", arr.sum() == 20 };
-  CRV_CONSTRAINT(x_constr, { x() > (1 << N), onehot(x()) });
-  CRV_CONSTRAINT(y_constr); //crv_constraint y_constr { "y_constr" };
-  CRV_CONSTRAINT(elem_constr);
-
-	item(crave::crv_object_name) { 
-    y_constr = { y() > arr.sum(), bitcount(y()) == N, y() % 2 == 1 };
-    for (int i = 0; i < N; i++)
-      elem_constr(0 < arr[i]() && arr[i]() < 10);
-	}   
-
-  void print() {
-    std::cout << "arr = ";
-    for (int i = 0; i < N; i++)
-      std::cout << " " << arr[i];
-    std::cout << std::endl;
-    std::cout << "x = " << x << " # " << std::bitset<64>(x) << std::endl;
-    std::cout << "y = " << y << " # " << std::bitset<64>(y) << std::endl;
-  }
-
 };
 
 int main (int argc , char *argv[]) {
   crave::init("crave.cfg");
 
-	item<5> it("item5");
-  assert(it.randomize());
-  it.print(); 
-
-	item<15> it1("item15");
-  assert(it1.randomize());
-  it1.print();
-
-  irqmp_item ii("irqmp_item");
-
-  my_covergroup cg("cg");
-  
-  cg.x.bind(ii.pending);
-  cg.y.bind(ii.priority);
-  
-  ii.goal(cg);
-  
-  for (int i = 0; i < 100; i++) {
-    assert(ii.randomize());    
+  irqmp_regs regs("regs");
+  irqmp_covergroup cg("cg");  
+  cg.lr.bind(regs.level_reg);
+  cg.fr.bind(regs.force_reg);
+  regs.goal(cg);
+  for (int i = 0; i < 50000; i++) {
+    assert(regs.randomize());    
     cg.sample();
     if (cg.covered()) {
       std::cout << "#Iterations = " << (i + 1) << std::endl;
       break;
     }
   }
-
   cg.report();
-
-	return 0;
 }
