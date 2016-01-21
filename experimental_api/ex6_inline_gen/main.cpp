d4d65398a9e3e7fe54291f700a5b46bc43ea3e15
Hoang M. Le <hle@informatik.uni-bremen.de>
2016-01-21 18:40:29 +0100

    use crave::solve as inline generator

--- experimental_api/ex6_inline_gen/main.cpp
+++ experimental_api/ex6_inline_gen/main.cpp
@@ -1,19 +1,15 @@
 #include <crave/ConstrainedRandom.hpp>
 #include <crave/experimental/Experimental.hpp>
 
-using crave::crv_constraint;
 using crave::crv_variable;
-using crave::randv;
-using crave::Generator;
 
 int main(int argc, char *argv[]) {
   crave::init("crave.cfg");
   crv_variable<int> x;
   crv_variable<int> y;
-  Generator gen;
-  gen(x() >= 0 && y() >= 0)(x() + y() == 4)(x() * y() == 4);
 
-  assert(gen.next());
+  assert( crave::solve(x() >= 0, y() >= 0, x() + y() == 4, x() * y() == 4) );
+
   std::cout << x << " " << y << std::endl;
 
   return 0;
