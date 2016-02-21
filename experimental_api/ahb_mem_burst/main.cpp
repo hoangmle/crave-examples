8dc265d0e6062dbfb146a1506d5da22c160a53fc
Niklas Bruns <nbruns@uni-bremen.de>
2016-02-21 15:35:43 +0100

    ahb_mem_burst to new api

--- experimental_api/ahb_mem_burst/main.cpp
+++ experimental_api/ahb_mem_burst/main.cpp
@@ -1,57 +1,34 @@
-#include "crave/ConstrainedRandom.hpp"
-
+#include <crave/ConstrainedRandom.hpp>
+#include <crave/experimental/Experimental.hpp>
 #include <boost/format.hpp>
 #include <iostream>
 
-using crave::rand_obj;
-using crave::randv;
-using crave::rand_vec;
-using crave::dist;
-using crave::distribution;
-using crave::inside;
-using crave::reference;
-using crave::placeholder;
-using crave::if_then;
-using crave::if_then_else;
-using crave::foreach;
+using namespace crave;
 
-enum kkind_e { SINGLE, INCR, WRAP4, INCR4, WRAP8, INCR8, WRAP16, INCR16 };
-CRAVE_ENUM(kkind_e, (SINGLE)(INCR)(WRAP4)(INCR4)(WRAP8)(INCR8)(WRAP16)(INCR16));
+CRAVE_BETTER_ENUM(kkind_e,SINGLE, INCR, WRAP4, INCR4, WRAP8, INCR8, WRAP16, INCR16);
+CRAVE_BETTER_ENUM(ssize_e,BYTE, HALFWORD, WORD);
 
-enum ssize_e { BYTE, HALFWORD, WORD };
-CRAVE_ENUM(ssize_e, (BYTE)(HALFWORD)(WORD));
-
-class ahb_burst : public rand_obj {
+class ahb_burst : public crv_sequence_item {
  public:
-  randv<kkind_e> kkind;
-  randv<ssize_e> ssize;
-  randv<unsigned int> incr_length;
-  randv<unsigned int> num_transfers;
-  randv<unsigned int> nnum_bytes;
-  randv<unsigned int> address;
+  crv_variable<kkind_e> kkind;
+  crv_variable<ssize_e> ssize;
+  crv_variable<unsigned int> incr_length;
+  crv_variable<unsigned int> num_transfers;
+  crv_variable<unsigned int> nnum_bytes;
+  crv_variable<unsigned int> address;
+  crv_constraint con;
+  crv_constraint word_aligned={crave::if_then(ssize() == ssize_e::WORD, address() % 4 == 0)};
+  crv_constraint halfword_aligned={crave::if_then(ssize() == ssize_e::HALFWORD, address() % 2 == 0)};
+  crv_constraint legal_incr_ssize={incr_length() > 0};
+  crv_constraint single_xfer={crave::if_then(kkind() == kkind_e::SINGLE, num_transfers() == 1)};
+  crv_constraint inc_xfer={crave::if_then(kkind() == kkind_e::INCR, num_transfers() == incr_length())};
+  crv_constraint B4={crave::if_then(kkind() == kkind_e::WRAP4 || kkind() == kkind_e::INCR4, num_transfers() == 4)};
+  crv_constraint B8={crave::if_then(kkind() == kkind_e::WRAP8 || kkind() == kkind_e::INCR8, num_transfers() == 8)};
+  crv_constraint B16={crave::if_then(kkind() == kkind_e::WRAP16 || kkind() == kkind_e::INCR16, num_transfers() == 16)};
 
-  ahb_burst(rand_obj* parent = 0)
-      : rand_obj(parent),
-        kkind(this),
-        ssize(this),
-        incr_length(this),
-        num_transfers(this),
-        nnum_bytes(this),
-        address(this) {
-
-    constraint("word_aligned", if_then(ssize() == WORD, address() % 4 == 0));
-    constraint("halfword_aligned", if_then(ssize() == HALFWORD, address() % 2 == 0));
-
-    constraint("legal_incr_ssize", incr_length() > 0);
-
-    constraint("single_xfer", if_then(kkind() == SINGLE, num_transfers() == 1));
-    constraint("inc_xfer", if_then(kkind() == INCR, num_transfers() == incr_length()));
-    constraint("4B", if_then(kkind() == WRAP4 || kkind() == INCR4, num_transfers() == 4));
-    constraint("8B", if_then(kkind() == WRAP8 || kkind() == INCR8, num_transfers() == 8));
-    constraint("16B", if_then(kkind() == WRAP16 || kkind() == INCR16, num_transfers() == 16));
-
+  ahb_burst(crave::crv_object_name){
     for (int i = 0; i < 3; i++) {
-      constraint(if_then(i == ssize(), nnum_bytes() == (num_transfers() << i)));
+      con&={(if_then(i == ssize(), nnum_bytes() == (num_transfers() << i)))};
     }
   }
 
@@ -65,34 +42,49 @@ class ahb_burst : public rand_obj {
   }
 };
 
-class mem_burst_16 : public rand_obj {
+class mem_burst_16 : public crv_sequence_item {
  public:
-  ahb_burst bursts[16];
-  randv<unsigned int> legal_size;
-  randv<unsigned int> bursts_nnum_bytes_acc[16];
+  ahb_burst bursts[16]={ahb_burst("0"),
+                        ahb_burst("1"),
+                        ahb_burst("2"),
+                        ahb_burst("3"),
+                        ahb_burst("4"),
+                        ahb_burst("5"),
+                        ahb_burst("6"),
+                        ahb_burst("7"),
+                        ahb_burst("8"),
+                        ahb_burst("9"),
+                        ahb_burst("10"),
+                        ahb_burst("11"),
+                        ahb_burst("12"),
+                        ahb_burst("13"),
+                        ahb_burst("14"),
+                        ahb_burst("15")
+                        };
+  crv_variable<unsigned int> legal_size;
+  crv_variable<unsigned int> bursts_nnum_bytes_acc[16];
+  crv_constraint legal_size_con={dist(legal_size(), distribution<uint>::simple_range(1, 16))};
+  crv_constraint con;
+  mem_burst_16(crave::crv_object_name) {
+    //for (int i = 0; i < 16; i++) 
+    //this->add_obj_child(&bursts[i]);
 
-  mem_burst_16(rand_obj* parent = 0) : rand_obj(parent), legal_size(this) {
-    for (int i = 0; i < 16; i++) 
-      this->add_obj_child(&bursts[i]);
-
-    constraint("legal_size", dist(legal_size(), distribution<uint>::simple_range(1, 16)));
-
-    constraint(bursts[0].address() == 0);
+    con&={(bursts[0].address() == 0)};
     for (int i = 1; i < 16; i++) {
-      constraint(bursts[i].address() == bursts[i - 1].address() + bursts[i - 1].nnum_bytes());
+      con&={(bursts[i].address() == bursts[i - 1].address() + bursts[i - 1].nnum_bytes())};
     }
 
     for (int i = 0; i < 16; i++) {
-      constraint(if_then(bursts[i].kkind() == 1, bursts[i].incr_length() <= 16));
+      con&={(if_then(bursts[i].kkind() == 1, bursts[i].incr_length() <= 16))};
     }
 
-    constraint(bursts_nnum_bytes_acc[0]() == bursts[0].nnum_bytes());
+    con&={(bursts_nnum_bytes_acc[0]() == bursts[0].nnum_bytes())};
     for (int i = 1; i < 16; i++) {
-      constraint(bursts_nnum_bytes_acc[i]() == bursts_nnum_bytes_acc[i - 1]() + bursts[i].nnum_bytes());
+      con&={(bursts_nnum_bytes_acc[i]() == bursts_nnum_bytes_acc[i - 1]() + bursts[i].nnum_bytes())};
     }
 
     for (int i = 0; i < 16; i++) {
-      constraint(if_then(legal_size() - 1 == i, bursts_nnum_bytes_acc[i]() == 16));
+      con&={(if_then(legal_size() - 1 == i, bursts_nnum_bytes_acc[i]() == 16))};
     }
   }
 };
@@ -100,12 +92,12 @@ class mem_burst_16 : public rand_obj {
 int main(int argc, char* argv[]) {
   crave::init("crave.cfg");
 
-  mem_burst_16 ahb_seq;
+  mem_burst_16 ahb_seq("item");
 
-  ahb_burst ab;
+  ahb_burst ab("burst");
 
   for (int i = 0; i < 5; i++) {
-    assert(ab.next());
+    assert(ab.randomize());
     std::cout << boost::format("ab[%u]") % i << std::endl;
     ab.print();
   }
@@ -113,7 +105,7 @@ int main(int argc, char* argv[]) {
   std::cout << "-----------" << std::endl;
 
   for (int i = 0; i < 10; i++) {
-    assert(ahb_seq.next());
+    assert(ahb_seq.randomize());
     std::cout << boost::format("burst_size = %d") % ahb_seq.legal_size << std::endl;
     for (int j = 0; j < ahb_seq.legal_size; j++) {
       ahb_seq.bursts[j].print();
